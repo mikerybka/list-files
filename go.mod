2f62158c66b9ffdd6ddb0ecb3b478723d7b549f5
Mike Rybka <merybka@gmail.com>
2025-04-09 10:54:54 -0400

    Create list-files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mikerybka/list-files
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	root := "."
+	if len(os.Args) > 2 {
+		root = os.Args[1]
+	}
+
+	// Walk the directory
+	err := filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() {
+			fmt.Println(path)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
+}
